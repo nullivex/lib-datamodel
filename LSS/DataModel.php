85bd09733a2f1b4a2a541b6c8b87e0259119f1d6
Nullivex <contact@nullivex.com>
2013-05-06 20:13:09 -0700

    initial commit

---
+++ LSS/DataModel.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ *  OpenLSS - Lighter Smarter Simpler
+ *
+ *	This file is part of OpenLSS.
+ *
+ *	OpenLSS is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as
+ *	published by the Free Software Foundation, either version 3 of
+ *	the License, or (at your option) any later version.
+ *
+ *	OpenLSS is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the 
+ *	GNU Lesser General Public License along with OpenLSS.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ModelService implements a callable object from an array of data
+ *	It can then be extended to implement formatters
+ *	This will most likely become an LSS package
+ */
+namespace LSS;
+use \Exception;
+
+class DataModel {
+
+	public $data;
+
+	public static function _setup($data){
+		$obj = new static();
+		$obj->_setData($data);
+		return $obj;
+	}
+
+	public function _setData($data){
+		if(!is_array($data))
+			throw new Exception('Model Service data must be an associative array');
+		$this->data = $data;
+		return $this;
+	}
+
+	public function _getAll(){
+		$row = array();
+		foreach(array_keys($this->data) as $col)
+			$row[$col] = call_user_func(array($this,self::_camelName($col,'get')));
+		return $row;
+	}
+
+	//example function would be getMyData which corresponds to $arr['my_data']
+	//	setting is possible with setMyData($val)
+	public function __call($name,$args){
+		if(strpos($name,'get') === 0){
+			$name = self::_realName($name,'get');
+			return mda_get($this->data,$name);
+		}
+		if(strpos($name,'set') === 0){
+			$name = self::_realName($name,'set');
+			return mda_set($this->data,$name,array_shift($args));
+		}
+		//we dont know what to do with this
+		return false;
+	}
+
+	//converts real_name into camelName
+	public static function _camelName($name,$prefix=null){
+		$name = str_replace('_',' ',$name);
+		$name = ucwords($name);
+		$name = str_replace(' ','',$name);
+		if(!is_null($prefix))
+			$name = lcfirst($name);
+		return $prefix.$name;
+	}
+
+	//this converts camel case into real_case
+	public static function _realName($name,$prefix=null){
+		$name = preg_replace('/^'.$prefix.'/','',$name);
+		$name = preg_replace('/([A-Z]{1})/','_$1',$name);
+		$name = strtolower($name);
+		$name = trim($name,'_');
+		return $name;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,90 @@
-lib-datamodel
+openlss/lib-datamodel
 =============
 
 Implements a callable object from an array of data that can be extended to implement inline formatting.
+
+Real Name
+===
+A real name users all lower case with underscores in place of spaces
+Example
+```
+$arr['my_real_name_here'] = 'foo';
+
+All data array members must use this naming.
+
+Camel Name
+===
+A camel name is generally used when calling functions
+Example
+```
+echo $this->getMyRealNameHere();
+```
+
+Both are used and generated in this class, so make sure they are properly provided and formatted.
+
+There are helper functions that can be used below to convert names.
+
+Usage
+===
+
+```php
+use \LSS\DataModel;
+
+class MyDataModel extends DataModel {
+
+	public function getFoo(){
+		return ucwords($this->data['foo']);
+	}
+
+}
+
+$row = array('foo'=>'test','bar'=>'foo');
+
+$obj = MyDataModel::_setup($row);
+var_dump($obj->getFoo()); //outputs 'Test'
+var_dump($obj->getBar()); //outputs 'foo'
+
+$obj->setFoo('test2');
+var_dump($obj->getFoo()); //outputs 'Test2'
+```
+
+Methods
+===
+
+$this DataModel::_setup($arr)
+==
+Sets the data array to use and instantiates the object
+NOTE: should be single dimensional associative array, 
+	multidimensional arrays will be treated as a
+	single dimensional array with array values
+
+$this DataModel::_setData($arr)
+==
+Used to set data same as above but on an existing object
+
+(array) DataModel::_getAll()
+==
+Returns an array similar to that used in _setData except all the values are passed through getters.
+Example
+```php
+$row = $obj->getAll();
+```
+
+(string) DataModel::_camelName($name,$prefix=null)
+==
+When passed a real_name it returns the camel name
+A prefix can be passed to get a usable function name
+Example
+```php
+var_dump(DataModel::_camelName('my_name','get')); //outputs 'getMyName'
+var_dump(DataModel::_camelName('my_name'); //outputs 'myName';
+```
+
+(string) DataModel::_realName($name,$prefix=null)
+==
+Used to obtain a real_name from a function name.
+Example
+```php
+var_dump(DataModel::_realName('getMyName','get')); //outputs 'my_name'
+var_dump(DataModel::_realName('myName')); //outputs 'my_name'
+```
